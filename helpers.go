cc05c74a123b175e88690b7086bd326ead4df193
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2024-12-31 23:59:46 +0330

    add New and Init methods

---
+++ helpers.go
@@ -0,0 +1,14 @@
+package quokka
+
+import "os"
+
+func (q *Quokka) createDirIfNotExist(path string) error {
+	const mode = 0755
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- quokka.go
+++ quokka.go
@@ -1,13 +1,35 @@
 package quokka
 
-func TestFunction(a, b int) int {
-	return a + b
+const version = "1.0.0"
+
+type Quokka struct {
+	AppName string
+	Debug   bool
+	Version string
 }
 
-func TestFunction2(a, b int) int {
-	return a - b
+func (quokka *Quokka) New(rootPath string) error {
+	initConfigs := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "middlewares", "logs", "public", "data", "migrations", "views", "tmp"},
+	}
+
+	err := quokka.Init(initConfigs)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func TestFunction3(a, b int) int {
-	return a * b
+func (quokka *Quokka) Init(path initPaths) error {
+	root := path.rootPath
+	for _, path := range path.folderNames {
+		err := quokka.createDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
