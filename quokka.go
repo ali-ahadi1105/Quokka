7c8fcf3c74e2e28dd5c5d64db8ecf2d6b258b0c7
aliah1105 <aliahadi.f1105@gmail.com>
2024-12-31 23:01:58 +0330

    Initia an project with golang

---
+++ quokka.go
@@ -0,0 +1,13 @@
+package quokka
+
+func TestFunction(a, b int) int {
+	return a + b
+}
+
+func TestFunction2(a, b int) int {
+	return a - b
+}
+
+func TestFunction3(a, b int) int {
+	return a * b
+}
