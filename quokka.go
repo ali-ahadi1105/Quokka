1a93aa5f0414b00d6650055643e5b972eef46253
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-01-19 08:51:30 +0330

    resolve problem to find .env file

--- quokka.go
+++ quokka.go
@@ -2,6 +2,9 @@ package quokka
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -9,9 +12,12 @@ import (
 const version = "1.0.0"
 
 type Quokka struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName  string
+	Debug    bool
+	Version  string
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+	RootPath string
 }
 
 func (quokka *Quokka) New(rootPath string) error {
@@ -20,22 +26,31 @@ func (quokka *Quokka) New(rootPath string) error {
 		folderNames: []string{"handlers", "middlewares", "logs", "public", "data", "migrations", "views", "tmp"},
 	}
 
+	// create folders structur of application
 	err := quokka.Init(initConfigs)
 
 	if err != nil {
 		return err
 	}
 
+	// check .env exist
 	err = quokka.checkDotenvFile(rootPath)
 	if err != nil {
 		return err
 	}
 
-	err = godotenv.Load(rootPath)
+	// load env file
+	err = godotenv.Load(rootPath + "/.env")
 	if err != nil {
 		return err
 	}
 
+	// create logs
+	infoLog, errorLog := quokka.startLoggers()
+	quokka.InfoLog = infoLog
+	quokka.ErrorLog = errorLog
+	quokka.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	quokka.Version = version
 	return nil
 }
 
@@ -56,4 +71,14 @@ func (quokka *Quokka) checkDotenvFile(path string) error {
 		return err
 	}
 	return nil
+}
+
+func (quokka *Quokka) startLoggers() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return infoLog, errorLog
 }
