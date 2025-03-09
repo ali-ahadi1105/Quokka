588be7d2ab1fb3160f57c70f87728a379c21d9bc
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-03-09 22:04:24 +0330

    add infoLog and errorLog

--- quokka.go
+++ quokka.go
@@ -2,6 +2,9 @@ package quokka
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -9,9 +12,12 @@ import (
 const version = "1.0.0"
 
 type Quokka struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName  string
+	Debug    bool
+	Version  string
+	InfoLog  *log.Logger
+	ErrorLog *log.Logger
+	RootPath string
 }
 
 func (q *Quokka) New(rootPath string) error {
@@ -36,6 +42,13 @@ func (q *Quokka) New(rootPath string) error {
 		return err
 	}
 
+	// create loggers
+	infoLog, errorLog := q.startLoggers()
+	q.InfoLog = infoLog
+	q.ErrorLog = errorLog
+	q.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	q.Version = version
+
 	return nil
 }
 
@@ -56,4 +69,14 @@ func (q *Quokka) checkDotEnvExist(path string) error {
 		return err
 	}
 	return nil
+}
+
+func (q *Quokka) startLoggers() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return infoLog, errorLog
 }
